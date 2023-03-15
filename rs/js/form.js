8e3f1b7bcaede3f7b4e0895dd209622d2aa27fed
Peace <padelamare@gmail.com>
2023-03-15 11:21:42 +0100

    style: connection
    connection info add background and illustration

--- rs/js/form.js
+++ rs/js/form.js
@@ -1,22 +1,73 @@
-const emailLogin = document.querySelector('#email-login');
-const labelEmailLogin = document.querySelector('#label-email-login');
-const passwordLogin = document.querySelector('#password-login');
-const labelPasswordLogin = document.querySelector('#label-password-login');
+// Effet de transform
 
-const labelsLogin = [labelEmailLogin,labelPasswordLogin];
-const inputsLogin = [emailLogin, passwordLogin]
+const connectionButton = document.querySelector("#connection-button");
 
-function verifyEmpty() {
-    inputsLogin.forEach((input,index) => {
-        if(input.value !== "") {
-            labelsLogin.index.style.transform = "translateY(-100%)"
-        }
-    })
-}
+connectionButton.textContent = "Nous contacter";
 
-inputsLogin.forEach((input) => {
-    input.addEventListener('input', (e) => {
-        verifyEmpty()
-        console.log("test")
-    })
-})
+const loginForm = document.querySelector(".login");
+const registerForm = document.querySelector(".register");
+
+connectionButton.addEventListener("click", () => {
+  if (connectionButton.classList.contains("register-button")) {
+    loginForm.classList.add("inactive-l");
+    registerForm.classList.add("active-r");
+
+    loginForm.classList.remove("active-l");
+    registerForm.classList.remove("inactive-r");
+
+    connectionButton.classList.add("login-button");
+    connectionButton.classList.remove("register-button");
+
+    // connectionButton text
+
+    connectionButton.textContent = "Se connecter";
+  } else {
+    loginForm.classList.add("active-l");
+    registerForm.classList.add("inactive-r");
+
+    loginForm.classList.remove("inactive-l");
+    registerForm.classList.remove("active-r");
+
+    connectionButton.classList.add("register-button");
+    connectionButton.classList.remove("login-button");
+
+    connectionButton.textContent = "Nous contacter";
+  }
+});
+
+// Effet des labels
+
+const labelsLogin = loginForm.querySelectorAll(".transform-label");
+const inputsLogin = loginForm.querySelectorAll("input");
+
+inputsLogin.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (input.value !== "") {
+      // S'il n'est pas vide
+      labelsLogin[index].style.transform = "translateY(-200%)";
+      labelsLogin[index].style.visibility = "visible";
+      labelsLogin[index].style.opacity = "1";
+    } else {
+      labelsLogin[index].style.transform = "translateY(-50%)";
+      labelsLogin[index].style.visibility = "hidden";
+      labelsLogin[index].style.opacity = "0";
+    }
+  });
+});
+
+// verification au début si autocomplete
+inputsLogin.forEach((input, index) => {
+    if(index <=1) {
+        if (input.value !== "") {
+            // S'il n'est pas vide
+            labelsLogin[index].style.transform = "translateY(-200%)";
+            labelsLogin[index].style.visibility = "visible";
+            labelsLogin[index].style.opacity = "1";
+          } else {
+            labelsLogin[index].style.transform = "translateY(-50%)";
+            labelsLogin[index].style.visibility = "hidden";
+            labelsLogin[index].style.opacity = "0";
+          }
+    }
+
+});
--- rs/js/message.js
+++ rs/js/message.js
@@ -1,11 +1,14 @@
-function displayMessage () {
-    fetch("https://remote.au-petit-primeur.fr:44301/api_pp/Message")
-  .then(response => response.text())
-  .then(data => {
-    const message =document.querySelector('#message');
-    message.textContent = data.DailyMessage
-  })
-  .catch(error => console.error(error));
+async function getDailyMessage() {
+  try {
+    const response = await fetch("https://remote.au-petit-primeur.fr:44301/api_pp/Message");
+    const data = await response.json();
+    const dailyMessage = data.DailyMessage;
+    const dailyMessageElement = document.getElementById('daily-message');
+    dailyMessageElement.innerText = dailyMessage;
+  } catch (error) {
+    console.error('Erreur lors de la récupération du message:', error);
+  }
 }
 
-displayMessage()
+getDailyMessage();
+
